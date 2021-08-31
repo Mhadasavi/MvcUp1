9b38df39d871a5e4b55dd8aa0dd74f137dcd9b24
Upkar Chaudhary <hexaview9@whiteoakuk.com>
2021-08-31 12:11:33 +0530

    Implement Image functionality

---
+++ MvcUp1/WC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcUp1
+{
+    public static class WC
+    {
+        public static string ImagePath = @"\Images\product\";
+    }
+}
