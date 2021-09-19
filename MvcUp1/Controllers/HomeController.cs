aa4c0c063a38c82d43d81b24053d9190620e37ae
Upkar <uchaudhary57@gmail.com>
2021-09-19 12:48:49 +0530

    Add to Cart & Remove from Cart functionality Added

--- MvcUp1/Controllers/HomeController.cs
+++ MvcUp1/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MvcUp1.Data;
 using MvcUp1.Models;
 using MvcUp1.Models.ViewModel;
+using MvcUp1.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -33,12 +35,57 @@ namespace MvcUp1.Controllers
         }
         public IActionResult Details(int id)
         {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null &&
+                HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
             DetailsViewModel detailsViewModel = new DetailsViewModel()
             {
                 Product = _db.Product.Include(u => u.Category).Include(u => u.Application)
-                .Where(u => u.Id == id).FirstOrDefault()
+                .Where(u => u.Id == id).FirstOrDefault(),
+                ExistInCart = false
             };
+            foreach (var item in shoppingCartList)
+            {
+                if (item.ProductId == id)
+                {
+                    detailsViewModel.ExistInCart = true;
+                }
+            }
             return View(detailsViewModel);
+        }
+        [HttpPost,ActionName("Details")]
+        public IActionResult DetailsPost([FromRoute] int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart)!=null &&
+                HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            shoppingCartList.Add(new ShoppingCart { ProductId = id });
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
+        }
+        
+        public IActionResult RemoveFromCart(int id)
+        {
+            List<ShoppingCart> shoppingCartsList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null &&
+                HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartsList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            var itemToRemove = shoppingCartsList.SingleOrDefault(r => r.ProductId == id);
+            if (itemToRemove != null)
+            {
+                shoppingCartsList.Remove(itemToRemove);
+            }
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartsList);
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
