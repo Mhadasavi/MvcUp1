90a14353a3c5be4f961593fa56db4d1070987181
Upkar <uchaudhary57@gmail.com>
2021-10-01 11:22:37 +0530

    Add Mailjet

---
+++ MvcUp1/Services/EmailSender.cs
@@ -0,0 +1,67 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcUp1.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        Task IEmailSender.SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailjetClient client = new MailjetClient(("97017448c2993fcf9fe49266b8135a24"), ("45c4b230001ea0e1a0b018a7f4e6843e"))
+            {
+                //Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", email},
+        {"Name", "Marcus"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          email
+         }, {
+          "Name",
+          "Marcus"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       "Greetings from Mailjet."
+      }, {
+       "TextPart",
+       "My first Mailjet email"
+      }, {
+       "HTMLPart",
+       "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!"
+      }, {
+       "CustomID",
+       "AppGettingStartedTest"
+      }
+     }
+             });
+            await client.PostAsync(request);
+        }
+    }
+}
