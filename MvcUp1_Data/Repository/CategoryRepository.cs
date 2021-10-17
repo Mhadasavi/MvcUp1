88d0d5d1a14cbea631596854ca49225c428648b3
Upkar <uchaudhary57@gmail.com>
2021-10-17 12:13:26 +0530

    Add Category Repo in Category Controller

---
+++ MvcUp1_Data/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using MvcUp1_Data.Repository.IRepository;
+using MvcUp1_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcUp1_Data.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db):base(db)
+        {
+            _db = db;
+        }
+        public void Update(Category obj)
+        {
+            var objFromDb = base.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.DisplayOrder = obj.DisplayOrder;
+            }
+        }
+    }
+}
